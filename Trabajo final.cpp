f973ef51720dfc64f91f25b6f2cbf05ee11d7319
rondon20193 <90071460+rondon20193@users.noreply.github.com>
2021-09-12 19:05:24 +0200

    Add files via upload

---
+++ Trabajo final.cpp
@@ -0,0 +1,21 @@
+void  (int inicial ){
+	int():
+		Q. push (nodo(inicial , 0) );
+		distancial[ inicial]=0;
+		int actual , adycebte , peso;
+		
+		while (! Q. e mp t y(){
+			actual = Q.top().first;
+			Q.pop();
+			if(visitado)[actual])continue;
+			
+			for(int i = 0; i < ady [actual].size;**i){
+				adyacente = ady [actual][i].first;
+				paso = ady [actual][i].second;
+				if(!visitado[adyacente]){
+				mi ni mazion( actual, adyacente, paso);
+				
+			
+			}
+		}
+}
